e08ba1a539e9aed96a56f0233ff87c2cb8d062f5
Federico Cano <canofedericomartin@gmail.com>
2018-03-22 00:40:38 -0300

    Setup babel, eslint, prettier, webpack, react

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  context: __dirname,
+  entry: [
+    'react-hot-loader/patch',
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    './app/ClientApp.jsx'
+  ],
+  devtool: 'cheap-eval-source-map',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+    publicPath: '/public/'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json']
+  },
+  devServer: {
+    hot: true,
+    publicPath: '/public/',
+    historyApiFallback: true
+  },
+  stats: {
+    color: true,
+    reasons: true,
+    chunks: true
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin(), new webpack.NamedModulesPlugin()],
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.jsx?$/,
+        loader: 'eslint-loader',
+        exclude: '/node_modules/'
+      },
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        options: {
+          plugins: ['transform-decorators-legacy']
+        }
+      }
+    ]
+  }
+};
