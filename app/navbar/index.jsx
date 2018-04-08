2b11c6873f025cd18d41148568edc7d2879c001a
Federico Cano <canofedericomartin@gmail.com>
2018-04-08 13:00:55 -0300

    Aligning navbar

--- app/navbar/index.jsx
+++ app/navbar/index.jsx
@@ -4,10 +4,12 @@ import NavMenu from './NavMenu';
 import DropdownUser from './DropdownUser';
 
 const NavBar = () => (
-  <div className="navbar navbar-expand-md navbar-dark bg-dark mb-0">
-    <Logo />
-    <NavMenu />
-    <DropdownUser />
+  <div className="navbar navbar-expand-md navbar-dark bg-primary mb-0">
+    <div className="container">
+      <Logo />
+      <NavMenu />
+      <DropdownUser />
+    </div>
   </div>
 );
 
