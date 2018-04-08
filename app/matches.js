4b6f15e004277094493967492a191c5d485dc204
Federico Cano <canofedericomartin@gmail.com>
2018-04-08 18:18:06 -0300

    Removing update status from matches json

--- app/matches.js
+++ app/matches.js
@@ -5,14 +5,14 @@ const ArabiaSaudita = makeTeam("sa", "Arabia Saudita");
 const Egipto = makeTeam("eg", "Egito");
 const Uruguay = makeTeam("uy", "Uruguay");
 
-const makeMatch = (checked, date, hour, homeTeam, homeBet, awayBet, awayTeam) => ({ checked, date, hour, homeTeam, homeBet, awayBet, awayTeam })
+const makeMatch = (date, hour, homeTeam, homeBet, awayBet, awayTeam) => ({ date, hour, homeTeam, homeBet, awayBet, awayTeam })
 const matches = [
-  makeMatch(true, "14/06", "12:00", Rusia, 0, 0, ArabiaSaudita),
-  makeMatch(false, "15/06", "9:00", Egipto, 0, 0, Uruguay),
-  makeMatch(true, "19/06", "15:00", Rusia, 3, 1, Egipto),
-  makeMatch(true, "20/06", "12:00", Uruguay, 2, 2, ArabiaSaudita),
-  makeMatch(false, "25/06", "11:00", Uruguay, 1, 3, Rusia),
-  makeMatch(false, "25/06", "11:00", ArabiaSaudita, 1, 1, Egipto)
+  makeMatch("14/06", "12:00", Rusia, 0, 0, ArabiaSaudita),
+  makeMatch("15/06", "9:00", Egipto, 0, 0, Uruguay),
+  makeMatch("19/06", "15:00", Rusia, 3, 1, Egipto),
+  makeMatch("20/06", "12:00", Uruguay, 2, 2, ArabiaSaudita),
+  makeMatch("25/06", "11:00", Uruguay, 1, 3, Rusia),
+  makeMatch("25/06", "11:00", ArabiaSaudita, 1, 1, Egipto)
 ];
 
 export default matches;
