8ebcc564cc69895105b993373ac8c8a65c970d82
Federico Cano <canofedericomartin@gmail.com>
2018-04-09 00:22:51 -0300

    SearchMatch

--- app/components/bets/SearchMatch.jsx
+++ app/components/bets/SearchMatch.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const SearchMatch = () => (
+const SearchMatch = (props) => (
   <div className="input-group ml-auto">
-    <input type="text" className="form-control" placeholder="Buscar..." />
+    <input type="text"
+      className="form-control"
+      placeholder="Buscar..."
+      onChange={props.handleSearchTermChange}
+      value={props.searchTerm}
+    />
   </div>
 );
+
+SearchMatch.defaultProps = {
+  searchTerm: '',
+  handleSearchTermChange: () => { }
+};
+
+SearchMatch.propTypes = {
+  searchTerm: PropTypes.string,
+  handleSearchTermChange: PropTypes.func
+};
 
 export default SearchMatch;
--- app/components/bets/index.jsx
+++ app/components/bets/index.jsx
@@ -1,19 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Filter from './filter';
 import SearchMatch from './SearchMatch';
 import Title from '../Title';
 import BetsTable from './betsTable';
 import matches from '../../matches';
 
-const Bets = () => (
-	<div>
-		<Title title='Listado de apuestas' />
-		<form className="form-inline">
-			<Filter />
-			<SearchMatch />
-		</form>
-		<BetsTable matches={matches} />
+const searchTeam = (team, currentSearch) => team.name.toUpperCase().includes(currentSearch.toUpperCase());
+const searchTeams = (home, away, currentSearch) => searchTeam(home, currentSearch) || searchTeam(away, currentSearch);
+
+const NotFound = () => (
+	<div className="jumbotron">
+		<h2 className="display-4 text-center">Sin resultados</h2>
+		<img className="rounded mx-auto d-block" src="app/images/notfound.png" alt="notfound" height="50%;" />
 	</div>
-);
+)
+
+class Bets extends Component {
+	state = {
+		searchTerm: ''
+	};
+	handleSearchTermChange = (event) => {
+		this.setState({ searchTerm: event.target.value });
+	};
+	render() {
+		const matchesFiltered = matches.filter(({ homeTeam, awayTeam }) => searchTeams(homeTeam, awayTeam, this.state.searchTerm));
+		const Table = matchesFiltered.length > 0 ? BetsTable : NotFound;
+		return (
+			<div>
+				<Title title='Listado de apuestas' />
+				<form className="form-inline">
+					<Filter />
+					<SearchMatch
+						searchTerm={this.state.searchTerm}
+						handleSearchTermChange={this.handleSearchTermChange}
+						{...this.props}
+					/>
+				</form>
+				<Table
+					matches={matchesFiltered}
+					{...this.props}
+				/>
+			</div>)
+	}
+};
 
 export default Bets;
